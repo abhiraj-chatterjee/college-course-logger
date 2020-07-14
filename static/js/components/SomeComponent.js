d016303c58efc6e40aaac0136266c4d98d84895e
abhiraj-chatterjee <abhirajchatterjee@gmail.com>
2020-07-15 03:48:15 +0530

    setup dev env

---
+++ static/js/components/SomeComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+//"../../img/react.png"
+import "./SomeStyle.css";
+
+const SomeComponent = () => {
+    return (
+        <div>
+            <h1 className="some-style-class">Hello, this is a react component.</h1>
+            <br />
+        </div>
+    );
+};
+
+export default SomeComponent;
